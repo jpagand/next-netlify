365d6bdcf58bf254d0954acc47cbe6aaaf7ccecd
jpagand <jordane21@gmail.com>
2018-11-28 18:09:36 +0100

    Added autre parrains

--- components/home/SectionParrains.js
+++ components/home/SectionParrains.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { Div } from '../Grid';
 import { Subheadline } from '../Typo';
 import Content from '../Content';
 import Images from '../Images';
 import Avatar from './Avatar';
+import ReactPlayer from 'react-player';
+import ReactModal from 'react-modal';
+
+const AutreParrain = styled(Div)`
+  cursor: pointer;
+  color: #5f5f5f;
+  text-decoration: underline;
+`
 
 class SectionParrains extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoUrl: null
+    };
+  }
+
+  onVideoClick = (videoUrl) => {
+    this.setState({videoUrl})
+  }
+
+  closeModal = () => {
+    this.setState({videoUrl: null})
+  }
+
   render() {
     const {titre, texte_intro, texte_outro, texte_autre_parrains, parrains = [], images, color,...etc} = this.props;
     const enAvant = parrains.filter(p => p.mettre_en_avant)
+    const autreParrains = parrains.filter(p => !p.mettre_en_avant)
     return (
       <Div row {...etc}>
         <Div colXs={12} colSm={6}>
@@ -24,12 +49,22 @@ class SectionParrains extends Component {
         <Div colXs={12} colSm={6}>
         <Div row>
           <Images images={images} color={color} colSm={12}/>
-            <Div colXs={12}>
-              <Content>{texte_autre_parrains}</Content>
+          <Div colXs={12}>
+            <Content>{texte_autre_parrains}</Content>
+            <Div row>
+            {autreParrains.map(parrain => (
+              <AutreParrain colXs={4} onClick={() => {parrain.lien_youtube && this.setState({videoUrl: parrain.lien_youtube})}}>
+                {parrain.nom}
+              </AutreParrain>
+            ))}
             </Div>
-          </Div>      
-        </Div>
-  
+           
+             <ReactModal isOpen={!!this.state.videoUrl} onRequestClose={this.closeModal}>
+              <ReactPlayer url={this.state.videoUrl} playing style={{margin: '0 auto'}}/>
+            </ReactModal>
+          </Div>
+        </Div>  
+      </Div>
       </Div>
     );
   }
